83b93e6289f62ad6c8b8dec51066543fec0fab60
Gautham R <gautham226@gmail.com>
2024-09-23 20:06:09 +0530

    student box commit

--- src/components/StudentBox.jsx
+++ src/components/StudentBox.jsx
@@ -3,36 +3,16 @@ import '../styles/StudentBox.css';
 
 function StudentBox({student}) {
     const [pres, setPres] = useState(false);
-    const [ab, setAb] = useState(false);
 
-
-    const handlePClick = () => {
-        pres === false ? setPres(true) : setPres(false);
-        if (ab === true) {
-            setAb(false);
-        }
-    }
-
-    const handleAClick = () => {
-        ab === false ? setAb(true) : setAb(false);
-        if (pres === true) {
-            setPres(false);
-        }
+    const handleClick = () => {
+        pres === true ? setPres(false) : setPres(true);
     }
 
     return(
         <div>
-            <div className="stud-box">
+            <div onClick={handleClick} className={ pres === false ? "stud-box" : "stud-box-clicked"}>
                 <div className="stud-name">
-                    <div>
-                        <h4>{student.first_name} {student.last_name}</h4>
-                    </div>
-                    <div onClick={handlePClick} className={pres === false ? "checkbox-present" : "checkbox-present-clicked"}>
-                        <h4>P</h4>
-                    </div>
-                    <div onClick={handleAClick} className={ab === false ? "checkbox-absent" : "checkbox-absent-clicked"}>
-                        <h4>A</h4>
-                    </div>
+                    <h4>{student.first_name} {student.last_name}</h4>
                 </div>
             </div>
         </div>
